50b06443aeeccd84b61058a6c447ab63cbd286f1
ppastram <>
2021-03-15 19:43:21 -0500

    Cursos y curso específico

--- rubricas/urls.py
+++ rubricas/urls.py
@@ -6,4 +6,5 @@ import rubricas.views.cursos as cursos
 urlpatterns = [
     path("home/", homes.home, name="home"),
     path("cursos/", cursos.cursos, name="cursos"),
+    path("cursos/especifico/", cursos.curso_especifico, name="especifico"),
     ]
--- rubricas/views/cursos.py
+++ rubricas/views/cursos.py
@@ -12,10 +12,14 @@ import rubricas.utils as utils
 def cursos(request):
     return cursos_estudiante(request)
 
-
 def cursos_estudiante(request):
     template_name = "rubricas/cursos/cursos_estudiante.html"
     context = {
     }
+    return utils.render_page(request, template_name, context)
 
+def curso_especifico(request):
+    template_name = "rubricas/cursos/especifico.html"
+    context = {
+    }
     return utils.render_page(request, template_name, context)
