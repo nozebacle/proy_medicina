c99a1d30fd204b750312a95d9d93c7044eeb6d1c
ppastram <>
2021-04-13 22:54:57 -0500

    Rubricas genérica y calificadas

--- rubricas/urls.py
+++ rubricas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("home/", homes.home, name="home"),
     path("cursos/", cursos.cursos, name="cursos"),
     path("cursos/especifico/", cursos.curso_especifico, name="especifico"),
+    path("cursos/rubrica_g/", cursos.rubrica_g, name="rubrica_g"),
+    path("cursos/rubrica_c/", cursos.rubrica_c, name="rubrica_c"),
     ]
--- rubricas/views/cursos.py
+++ rubricas/views/cursos.py
@@ -2,13 +2,9 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404, HttpResponse
 
 import json
-
 import rubricas.utils as utils
-#from rubricas.models.usuarios import Usuario
-#from rubricas.constantes import PROFESOR, ESTUDIANTE, ADMINISTRADOR, MONITOR, COORDINADOR, SEMESTRE_ACTUAL
 
 
-#def home(request):
 def cursos(request):
     return cursos_estudiante(request)
 
@@ -20,6 +16,18 @@ def cursos_estudiante(request):
 
 def curso_especifico(request):
     template_name = "rubricas/cursos/especifico.html"
+    context = {
+    }
+    return utils.render_page(request, template_name, context)
+
+def rubrica_g(request):
+    template_name = "rubricas/cursos/rubrica_g.html"
+    context = {
+    }
+    return utils.render_page(request, template_name, context)
+
+def rubrica_c(request):
+    template_name = "rubricas/cursos/rubrica_c.html"
     context = {
     }
     return utils.render_page(request, template_name, context)
